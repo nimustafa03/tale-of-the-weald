8bbec3b84952aeccc9a933bdac3e2368d3542a33
nico <nimustafa03@gmail.com>
2025-07-09 18:51:35 -0300

    agregué un par de cosas pero la verdad me olvidé qué
    ee le metí un addon para hacer greyboxing más tranqui y empecé a meter un enemigo

--- scripts/health_label.gd
+++ scripts/health_label.gd
@@ -4,4 +4,4 @@ extends Label
 @onready var healthComponent : HealthComponent 
 
 func _process(delta):
-	text = "%s/" %parent.healthComponent.max_health + "%s" %parent.healthComponent.health
+	text = "%s" %parent.healthComponent.health + "%s/" %parent.healthComponent.max_health 
