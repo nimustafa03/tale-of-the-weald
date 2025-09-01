74849bd8c06ce71899b23341759863cb23ef8bb0
nico <nimustafa03@gmail.com>
2025-09-01 12:26:30 -0300

    Añadido un coleccionable genérico.

--- scripts/components/hitbox_component.gd
+++ scripts/components/hitbox_component.gd
@@ -2,7 +2,9 @@ extends Area3D
 class_name HitboxComponent
 
 @export var health_component : HealthComponent
+@export var collectable_component : CollectableComponent
 
 func damage(attack : Attack):
 	if health_component:
 		health_component.damage(attack)
+	
--- scripts/genericCollectable.gd
+++ scripts/genericCollectable.gd
@@ -1,2 +1,10 @@
-extends StaticBody3D
+extends Node
 class_name GenericCollectable
+
+@export var parent : StaticBody3D
+
+func _on_collection_component_area_entered(area):
+	if area is HitboxComponent:
+		var hitbox : HitboxComponent = area
+		hitbox.collectable_component.addCollectableAmount("Primitive",1)
+		parent.queue_free()
